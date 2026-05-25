a158567ffe170c081501e2cdae53d11594d6f9be
Sumit Kawachale <sumit.study.1603@gmail.com>
2026-05-25 08:44:26 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/atlasstay/backend/AtlasstayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atlasstay.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasstayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasstayBackendApplication.class, args);
+	}
+
+}
